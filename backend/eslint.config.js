e3425841c3667e80fb2eedb06267806bcb1c521e
KodsLamouchi322 <lamouchikods42@gmail.com>
2026-05-01 14:51:47 +0100

    fix: update eslint config to v9

---
+++ backend/eslint.config.js
@@ -0,0 +1,18 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        process: "readonly",
+        console: "readonly",
+        __dirname: "readonly"
+      }
+    }
+  }
+];
